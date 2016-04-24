93478347fb47a9abcc73bde167daa5d6746fe721
Matias Piipari <matias.piipari@gmail.com>
2016-04-24 01:56:25 +0100

    Adds a Carthage compatible framework target and tweaks .gitignore.

---
+++ DORateLimitKit/DORateLimitKit/DORateLimitKit.h
@@ -0,0 +1,19 @@
+//
+//  DORateLimitKit.h
+//  DORateLimitKit
+//
+//  Created by Matias Piipari on 24/04/2016.
+//  Copyright © 2016 Matias Piipari. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DORateLimitKit.
+FOUNDATION_EXPORT double DORateLimitKitVersionNumber;
+
+//! Project version string for DORateLimitKit.
+FOUNDATION_EXPORT const unsigned char DORateLimitKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DORateLimitKit/PublicHeader.h>
+
+
